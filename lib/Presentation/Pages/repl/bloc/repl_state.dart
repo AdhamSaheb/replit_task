dc3df64f42aa967157c7f0617d34e15fcdc5a9a6
adham <adhamst1212@gmail.com>
2021-12-02 10:27:15 +0200

    Added state and event

---
+++ lib/Presentation/Pages/repl/bloc/repl_state.dart
@@ -0,0 +1,35 @@
+part of 'repl_bloc.dart';
+
+abstract class ReplState {}
+
+class ReplInitian extends ReplState {}
+
+class ReplLoading extends ReplState {
+  @override
+  String toString() {
+    return 'ReplLoading';
+  }
+}
+
+class ReplLoaded extends ReplState {
+  final String code;
+  final String runResult;
+
+  ReplLoaded({required this.code, required this.runResult});
+
+  @override
+  String toString() {
+    return 'ReplLoaded';
+  }
+}
+
+class ReplFailed extends ReplState {
+  final String errorMessage;
+
+  ReplFailed({required this.errorMessage});
+
+  @override
+  String toString() {
+    return 'ReplFailed';
+  }
+}
